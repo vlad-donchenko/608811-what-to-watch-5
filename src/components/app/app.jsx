9f09220d66facae30f0facb72bda9d36847eaa45
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-10-06 00:06:28 +0300

    connect Router and create login component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
+import Login from "../login/login.jsx";
 
 const App = ({movie}) => {
-  return <Main movie={movie}/>;
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <Main movie={movie}/>
+          )}
+        />
+        <Route
+          exact
+          path="/login"
+          render={() => (
+            <Login/>
+          )}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
