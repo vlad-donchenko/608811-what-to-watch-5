e3c8d38e285b2a5ac04a37724a40e4ea5ad521c7
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-10-06 11:10:20 +0300

    create movie and fixes routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,6 +3,8 @@ import {BrowserRouter, Switch, Route} from "react-router-dom";
 import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
 import Login from "../login/login.jsx";
+import MyList from "../my-list/my-list.jsx";
+import Movie from "../movie/movie.jsx";
 
 const App = ({movie}) => {
 
@@ -21,6 +23,20 @@ const App = ({movie}) => {
           path="/login"
           render={() => (
             <Login/>
+          )}
+        />
+        <Route
+          exact
+          path="/mylist"
+          render={() => (
+            <MyList/>
+          )}
+        />
+        <Route
+          exact
+          path="/films/:id"
+          render={(ownProps) => (
+            <Movie ownProps={ownProps}/>
           )}
         />
       </Switch>
